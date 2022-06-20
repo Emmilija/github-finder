895bc5cbab16530e004d3508d67d7e781f879889
Emilija Karatashesvka <karatashevskaemilija@gmail.com>
2022-06-20 17:10:03 +0200

    initial

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import UserResault from "../components/users/UserResault"
+import UserSearch from "../components/users/UserSearch"
+
+function Home() {
+  return (
+    <>
+        <UserSearch />
+        <UserResault />
+    </>
+  )
+}
+
+export default Home
